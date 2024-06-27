7c8f6b812bfb16cb8d9aff48be1b79cc8fa85c0c
a-chabot <a.chabot@salesforce.com>
2024-06-27 02:02:22 -0400

    functional example

--- src/modules/x/child/child.js
+++ src/modules/x/child/child.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    get linksExample() {
+        return '<p> this is example.com and salesforce.com </p>';
+    }
+}
